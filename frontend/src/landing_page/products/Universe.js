8b4d69df4699ef996aa542296bc91a1fcc086a2d
Qaiyum12 <abdulqaiyum710636@gmail.com>
2025-10-12 01:17:41 +0600

    initial commit

---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1 className="mb-3">The TradeVerse Universe</h1>
+        <p>xtend your trading and investment experience even further with our partner platforms</p>
+        
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" className="mb-3" />
+          <p className="text-small text-muted">Thematic investing platform.</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5 " >
+         <img src="media/images/streakLogo.png" className="mb-3" style={{width: "40%"}}/>
+          <p className="text-small text-muted">Algo & strategy platform.</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media\images\sensibullLogo.svg" className="mb-3" style={{width: "40%"}}/>
+          <p className="text-small text-muted">Options trading platform.</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media\images\zerodhaFundhouse.png" className="mb-3" style={{width: "40%"}}/>
+          <p className="text-small text-muted">Asset management.</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+         <img src="media/images/goldenpiLogo.png" className="mb-3" style={{width: "40%"}}/>
+          <p className="text-small text-muted">Bonds trading platform.</p>
+        </div>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/dittoLogo.png" className="mb-3" style={{width: "40%"}}/>
+          <p className="text-small text-muted">Insurance.</p>
+        </div>
+
+        <button className='p-2 btn btn-primary fs-5 mb-5 mt-5' style={{width: "20%", margin: "0 auto"}}>Signup now</button>
+      </div>
+    </div>
+  );
+}
+
+export default Universe;
---
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+function CreateTicket() {
+  return (
+    <div className="container">
+      <div className="row p-5 mb-5 mt-5">
+        <h1 className="fs-2">To Create a ticket, select a relevant topic</h1>
+
+        <div className="col-4 p-5 mb-2 mt-2">
+          <h4 className="mb-5"> <i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opennig</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Online Account Opening</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Offline Account Opening</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Company, Partnership and HUF Account Opening</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>NRI Account Opening</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Charges at TradeVerse</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>TradeVerse IDFC FIRST Bank 2-in-1 Account</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Getting Started</a>
+
+        </div>
+
+        <div className="col-4 pt-5 mb-2 mt-2">
+          <h4 className="mb-5"><i class="fa fa-user" aria-hidden="true"></i> Your TradeVerse Account</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Login Credentials</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Account Modification and Segment Addition</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>DP ID and bank details</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Your Profile</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Transfer and conversion of shares</a> <br/>
+        </div>
+
+        <div className="col-4 p-5 mb-2 mt-2">
+          <h4 className="mb-5"><i class="fa fa-bar-chart" aria-hidden="true"></i> Your TradeVerse Account</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Margin/leverage, Product and Order types</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Kite Web an Mobile</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Trading FAQs</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Corporate Actions</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Sentinel</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Kite API</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Pi and other platforms</a>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Stockreports+</a>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>GTT</a>
+        </div>
+
+        <div className="col-4 p-5 mb-2 mt-2">
+          <h4 className="mb-5"> <i class="fa fa-credit-card" aria-hidden="true"></i> Funds</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Adding Funds</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Funds Withdrawal</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>eMandates</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Adding Bank Accounts</a> <br/>
+
+        </div>
+
+        <div className="col-4 p-5 mb-2 mt-2">
+          <h4 className="mb-5"> <i class="fa fa-circle-o-notch" aria-hidden="true"></i> Console</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Reports</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Ledger</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Portfolio</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>60 Day Challenge</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>IPO</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Referral Program</a> <br/>
+
+        </div>
+
+        <div className="col-4 p-5 mb-2 mt-2">
+          <h4 className="mb-5"> <i class="fa fa-circle-o" aria-hidden="true"></i> Coin</h4>
+
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Understanding Mutual Funds</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>About Coin</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Buying and Selling through Coin</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Starting an SIP</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Managing your Portfolio</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Coin App</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Moving to Coin</a> <br/>
+          <a href="" style={{textDecoration: "none", lineHeight: "2.5"}}>Government Securities</a> 
+
+        </div>       
+        
+      </div>
+    </div>
+  );
+}
+
+export default CreateTicket;
